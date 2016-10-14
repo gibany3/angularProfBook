66b1f03ff9d601523eac7bc6c6791676791e9e69
mjarosz <mjarosz@pgs-soft.com>
2016-10-14 07:42:34 +0200

    Page 153

--- angularjs/filters/customFilters.js
+++ angularjs/filters/customFilters.js
@@ -1,4 +1,4 @@
-angular.module("custonFilters", [])
+angular.module("customFilters", [])
     .filter("unique", function () {
         return function (data, propertyName) {
             if (angular.isArray(data) && angular.isString(propertyName)) {
@@ -7,12 +7,38 @@ angular.module("custonFilters", [])
                 for (var i = 0; i < data.length; i++) {
                     var val = data[i][propertyName];
                     if (angular.isUndefined(keys[val])) {
-                        keys[val = true;
+                        keys[val] = true;
                         results.push(val);
-                    ]
                     }
                 }
                 return results;
+            } else {
+                return data;
+            }
+        }
+    })
+    .filter("range", function ($filter) {
+        return function (data, page, size) {
+            if (angular.isArray(data) && angular.isNumber(page) && angular.isNumber(size)) {
+                var start_index = (page - 1) * size;
+                if (data.length < start_index) {
+                    return [];
+                } else {
+                    return $filter("limitTo")(data.splice(start_index), size);
+                }
+            } else {
+                return data;
+            }
+        }
+    })
+    .filter("pageCount", function () {
+        return function (data, size) {
+            if (angular.isArray(data)) {
+                var result = [];
+                for (var i = 0; i < Math.ceil(data.length / size) ; i++) {
+                    result.push(i);
+                }
+                return result;
             } else {
                 return data;
             }
